230e68587450c65671a7e73b19bd72c8cb60c0e2
GledistonFerreira <135056224+glediston@users.noreply.github.com>
2025-03-29 21:45:19 -0300

    .gitignore
    o meu projeto por segurança esta ignorando a configuração do banco de dados

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const app = express();
 const PORT = 3001;
+const sequelize = require('../backend/config/db')
 
 app.listen(3001, () =>{
     console.log(`Servidor rodando na porta ${ PORT}`)
 })
+
+sequelize.authenticate()
+    .then(() => {
+      console.log(' Conectado ao banco de dados!');
+      // Sincronizando os modelos com o banco de dados
+      
+    })
+    .catch(err => console.error('Erro ao conectar ao banco de dados:', err));
+  
